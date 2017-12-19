142d0965127f00f1ffda092555bfb646c9dfbfc9
GlennJC <glenn.j.corbett@gmail.com>
2017-12-20 08:04:21 +1100

    Minor Updates

--- HSD Build/HSD Build - Minimal.ps1
+++ HSD Build/HSD Build - Minimal.ps1
@@ -4,6 +4,7 @@ $labSources = Get-LabSourcesLocation
 
 New-LabDefinition -Name $LabName -DefaultVirtualizationEngine HyperV -VmPath "C:\Hyper-V\Automation-Lab"
 Add-LabVirtualNetworkDefinition -Name $LabName -AddressSpace 192.168.60.0/24
+Add-LabIsoImageDefinition -Name Office2013 -Path $labSources\ISOs\en_office_professional_plus_2013_x86_x64_dvd_1135709.iso
 
 $PSDefaultParameterValues = @{
     'Add-LabMachineDefinition:Network' = $labName
@@ -15,9 +16,8 @@ $PSDefaultParameterValues = @{
 
 
 Add-LabMachineDefinition -Name HSD-DC01 -Memory 2GB -Roles RootDC -IPAddress '192.168.60.10'
-Add-LabMachineDefinition -Name HSD-HSD1 -Memory 2GB -IPAddress '192.168.60.20'
-Add-LabMachineDefinition -Name HSD-HSD2 -Memory 2GB -IPAddress '192.168.60.21'
-
+#Add a host that will run all the roles.  Since we want to demonstrate both published desktops and apps, install Office
+Add-LabMachineDefinition -Name HSD-HSD1 -Memory 4GB -IPAddress '192.168.60.20' -Roles Office2013
 
 #Do the installation
 Install-Lab
@@ -32,25 +32,19 @@ Invoke-LabCommand -ActivityName 'Configure Remote Desktop Services' -ComputerNam
 
 Restart-LabVM -ComputerName 'HSD-HSD1' -Wait
 
-Invoke-LabCommand -ActivityName 'Add RD Licensing Server' -ComputerName 'HSD-DC01' -ScriptBlock {
+Invoke-LabCommand -ActivityName 'Configure RDS' -ComputerName 'HSD-DC01' -ScriptBlock {
     Import-Module RemoteDesktop
-    Add-RDServer -Server 'HSD-HSD2.hsdlab.local' -Role RDS-RD-SERVER -ConnectionBroker 'HSD-HSD1-hsdlab.local'
+    Import-Module ActiveDirectory
     Add-RDServer -Server 'HSD-DC01.hsdlab.local' -Role RDS-LICENSING -ConnectionBroker 'HSD-HSD1.hsdlab.local'
     Set-RDLicenseConfiguration -LicenseServer 'HSD-DC01.hsdlab.local' -Mode PerUser -ConnectionBroker 'HSD-HSD1.hsdlab.local' -Force
-}
+    #Need to add the License server to the 'Terminal Server License Servers' group in AD
+    Add-ADGroupMember 'Terminal Server License Servers' 'HSD-DC01$'
+    New-RDSessionCollection –CollectionName 'SessionCollection' –SessionHost 'HSD-HSD1.hsdlab.local' –CollectionDescription 'Desktop Session Collection' –ConnectionBroker 'HSD-HSD1.hsdlab.local'  
 
-#Add A Session Host Collection (Full Desktop) on the first Server
-Invoke-LabCommand -ActivityName 'Add Session Collection' -ComputerName 'HSD-HSD1' -ScriptBlock {
-    Import-Module RemoteDesktop
-    New-RDSessionCollection –CollectionName SessionCollection –SessionHost 'HSD-HSD1.hsdlab.local' –CollectionDescription 'Desktop Session Collection' –ConnectionBroker 'HSD-HSD1.hsdlab.local'  
+    New-RDRemoteapp -Alias Wordpad -DisplayName WordPad -FilePath "C:\Program Files\Windows NT\Accessories\wordpad.exe" -ShowInWebAccess 1 -CollectionName 'SessionCollection' -ConnectionBroker 'HSD-HSD1.hsdlab.local'  
+    
 }
-
-#Add A RemoteApp Collection on the second Server
-Invoke-LabCommand -ActivityName 'Add RemoteApp Collection' -ComputerName 'HSD-HSD2' -ScriptBlock {
-    Import-Module RemoteDesktop
-    New-RDSessionCollection –CollectionName RemoteAppCollection –SessionHost 'HSD-HSD2.hsdlab.local' –CollectionDescription 'RemoteApp Session Collection' –ConnectionBroker 'HSD-HSD1.hsdlab.local'
-}
-   
+  
 Checkpoint-LABVM -All -SnapshotName 'After RDS Install'
       
 Show-LabDeploymentSummary -Detailed
