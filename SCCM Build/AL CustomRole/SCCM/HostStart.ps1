bb5bd915a176266cbf31f533d102674764c8c2fe
Raimund Andée [MSFT] <r.andree@live.com>
2018-04-13 17:21:10 +0200

    Adding OS version test

--- SCCM Build/AL CustomRole/SCCM/HostStart.ps1
+++ SCCM Build/AL CustomRole/SCCM/HostStart.ps1
@@ -19,6 +19,20 @@ param(
     [string]$SqlServerName
 )
 
+$sqlServer = Get-LabVM -Role SQLServer | Where-Object Name -eq $SqlServerName
+$sccmServer = Get-LabVM -ComputerName $SccmServerName
+if (-not $sqlServer)
+{
+    Write-Error "The specified SQL Server '$SqlServerName' does not exist in the lab."
+    return
+}    
+
+if ($sccmServer.OperatingSystem.Version -lt 10.0 -or $SqlServer.OperatingSystem.Version -lt 10.0)
+{
+    Write-Error "The SCCM role requires the SCCM server and the SQL Server to be Windows 2016 or higher."
+    return
+}
+
 $script = Get-Command -Name $PSScriptRoot\DownloadAdk.ps1
 $param = Sync-Parameter -Command $script -Parameters $PSBoundParameters
 & $PSScriptRoot\DownloadAdk.ps1 @param
